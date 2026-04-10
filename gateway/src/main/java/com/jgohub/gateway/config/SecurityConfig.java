16b05dd2cc4e5c442134c73bd87a28a2908afca7
Mouhamad DJIGO <mehmetdjigo@gmail.com>
2026-04-10 01:16:12 +0000

    gateway qui refuse de demarrer

--- gateway/src/main/java/com/jgohub/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/jgohub/gateway/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableReactiveMethodSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
